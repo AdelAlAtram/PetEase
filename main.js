358e1d2a8d0c3606814dff100e1c0defea83ce55
AdelAlAtram <adelaaymann@gmail.com>
2024-03-17 21:31:12 +0300

    cloning

---
+++ main.js
@@ -0,0 +1,586 @@
+
+
+
+
+let categoty = [
+    {
+        itemsOf: "food",
+        img: "https://ipcdn.freshop.com/resize?url=https://images.freshop.com/00050000170180/e3676ba46036ecdcc006b2d7a0312a5c_large.png&width=256&type=webp&quality=80",
+        paragraf : "Friskies Gravy",
+        price : "3 JOD"
+    },
+   
+    {      
+        itemsOf: "food",
+        paragraf : "Friskies Chicken and turkey",
+        img: "https://ipcdn.freshop.com/resize?url=https://images.freshop.com/00050000573950/9765a4fefc27782ac5673dd77bd24cef_large.png&width=256&type=webp&quality=80",
+        price : "3 JOD"
+    },
+    {
+        itemsOf: "food",
+        img: "https://ipcdn.freshop.com/resize?url=https://images.freshop.com/00050000502189/547f1d102dbbb40a74e3ad9efa19a106_large.png&width=256&type=webp&quality=80",
+        paragraf : "Friskies Dry Food",
+        price : "3 JOD"
+    },
+   
+    {      
+        itemsOf: "food",
+        paragraf : "Friskies Dry Food ",
+        img: "https://ipcdn.freshop.com/resize?url=https://images.freshop.com/00050000168583/9234f3fe6baceb70ef0b2e5d0d5bba7e_large.png&width=256&type=webp&quality=80",
+        price : "3 JOD"
+    },
+    {
+        itemsOf: "food",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSijYiAIXvbXJXnM48VWGbMdyKuo0qnBwVfaw&usqp=CAU",
+        paragraf : "Friskies Treats",
+        price : "3 JOD"
+    },
+   
+    {      
+        itemsOf: "food",
+        paragraf : "Friskies chicken",
+        img: "https://ipcdn.freshop.com/resize?url=https://images.freshop.com/00050000170180/e3676ba46036ecdcc006b2d7a0312a5c_large.png&width=256&type=webp&quality=80",
+        price : "3 JOD"
+    },
+
+    {
+        itemsOf: "grooming",
+        paragraf : "Ear Wash",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXWPpG3q7iap6glYR7DF-LYxJ-scAp1NT_SA&usqp=CAU",
+        price : "3 JOD"
+    },
+
+    {
+        itemsOf: "grooming",
+        paragraf : "Top Dog",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwzGtdI4raskz4qLdGPzOVFtTxsclSGGqpkQ&usqp=CAU",
+        price : "3 JOD"
+    },
+    {
+        itemsOf: "grooming",
+        paragraf : "Bluestem",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-PLmTokDSAIYHxDX8JifY2K67zAbpZ_bsOw&usqp=CAU",
+        price : "3 JOD"
+    },
+
+    {
+        itemsOf: "grooming",
+        paragraf : "Ear Wash",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXWPpG3q7iap6glYR7DF-LYxJ-scAp1NT_SA&usqp=CAU",
+        price : "3 JOD"
+    },
+    {
+        itemsOf: "grooming",
+        paragraf : "Lush Me",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRDDfZ6uR49N0Pf7qgihpGwYWtw79kNSODPqg&usqp=CAU",
+        price : "3 JOD"
+    },
+
+    {
+        itemsOf: "grooming",
+        paragraf : "Ear Wash",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQXWPpG3q7iap6glYR7DF-LYxJ-scAp1NT_SA&usqp=CAU",
+        price : "3 JOD"
+    },
+
+    {
+        itemsOf: "toys",
+        paragraf : "pet toy",
+        img: "https://www.ubookstore.com/site/product_images/Ty-Slush-Squishy-Beanie-Boos-Plush-Clip_Main-1.jpg?resizeid=7&resizeh=225&resizew=225",
+        price : "3 JOD"
+    },
+
+    {
+        itemsOf: "toys",
+        paragraf : "pet toy",
+        img: "https://ipcdn.freshop.com/resize?url=https://images.freshop.com/00008421368464/11b475180a739fb2d6b2d57154d89bc2_large.png&width=256&type=webp&quality=80",
+        price : "3 JOD"
+    },
+    {
+        itemsOf: "toys",
+        paragraf : "pet toy",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCJa7hGnIcKsUPEIwJcGQDTV6eXhzjgKj5VOBvL21-xdmt_XEaa--tDhYDoj87oKqsoM4&usqp=CAU",
+        price : "3 JOD"
+    },
+
+    {
+        itemsOf: "toys",
+        paragraf : "pet toy",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-1NUsvZzxbgYD_jrTzyjsm9vSc9KKTiw80Q&usqp=CAU",
+        price : "3 JOD"
+    },
+    {
+        itemsOf: "toys",
+        paragraf : "pet toy",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp9QHWWZ5_haaHIxbyH5FTzDmpKI5fy3gRzQ&usqp=CAU",
+        price : "3 JOD"
+    },
+
+    {
+        itemsOf: "toys",
+        paragraf : "pet toy",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwooZsIqJnQ7B2IwqtLAUOO4igtlSSiq4MQFUYaZlHUPhAjQ4rJyvTvGdx9wi_06Yk-Pg&usqp=CAU",
+        price : "3 JOD"
+    },
+]
+
+
+const cardsArr = JSON.parse(localStorage.getItem("card")) || []
+
+
+
+
+// forms
+
+
+const users = [
+    {
+        name:"Ahmad",
+        password: "179179"
+    },
+    {
+        name:"Adel",
+        password: "119119"
+    }
+]
+
+
+const inputs = $(".inputs")
+const inputs2 = $(".inputs2")
+const loginform = $(".loginform")
+const signupform = $(".signupform")
+loginform.hide()
+signupform.hide()
+
+
+
+const login = $("#login")
+const signup = $("#signup")
+login.on("click", () => {
+    hide.hide()
+    signupform.hide()
+    loginform.show()
+ });
+
+ signup.on("click", () => {
+    hide.hide()
+    loginform.hide()
+    signupform.show()
+ });
+
+ const clickOn1 = $(".clickOn1")
+ clickOn1.on("click", () => {
+    loginform.hide()
+    signupform.show()
+ });
+
+
+const clickOne = $(".clickOne")
+const loginf = function (){
+    for(let i = 0 ; i < users.length ; i++){
+        if(inputs.val() === users[i].name && inputs2.val() === users[i].password){
+            loginform.hide()
+            hide.show()
+            signupform.hide()
+            return 
+        }
+        else{
+            loginform.hide()
+            signupform.show()
+        }
+    }
+}
+
+clickOne.on("click", () => {
+    loginf()
+ });
+
+
+// let newArr = [{
+
+// }]
+
+const clickTwo = $(".clickTwo")
+const namee = $(".namee")
+const passwordd = $(".passwordd")
+const signupf = function (){
+    // newArr = namee.val(),
+    console.log(namee.val());
+    users.push({name : namee.val(),password:passwordd.val()})
+}
+
+
+
+
+ clickTwo.on("click", () => {
+    signupf()
+    console.log(users);
+ });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  Element.categoty === categury button value
+
+// const newDiv = $(".iteminfo")
+// const newDivItems = $("<h1>Tune in to the Make it Big Podcast — our thought leadership audio series for retailers, entrepreneurs and ecommerce professionals. You'll get expert insights, strategies and tactics to help grow your business.</h1>")
+// newDiv.append(newDivItems)
+// const newBtn1 = $("<button>HOME</button>")
+// newDiv.append(newBtn1)
+
+
+// newDiv.hide()
+
+const firstbtn = $(".first")
+const secbtn = $(".sec")
+const therdbtn = $(".therd")
+// const readmore = $(".readmore")
+const hide = $(".hide")
+const btn2 = $(".clicker2")
+const btn3 = $(".clicker3")
+const btn4 = $(".clicker4")
+const itemss = $(".items")
+
+
+const hideItems = function(){
+    hide.hide()
+    newDiv.show()
+}
+
+// readmore.on("click", () => {
+//      hideItems()
+//   });
+
+const showItems = function(){
+    newDiv.hide()
+    hide.show()
+}
+
+// newBtn1.on("click", () => {
+//     showItems()
+//   });
+
+const container=$(".detail")
+const navv=$("#navv")
+// .addClass("intro")
+
+
+const info=$(".info")
+const insideInfo=$(".insideInfo")
+const broductPic=$(".broductPic")
+const broductinfo=$(".broductinfo")
+const namename=$(".namename")
+const infobtn=$(".infobtn")
+
+info.hide()
+
+const items = function(){
+for(let i = 0; i < categoty.length; i++){
+    console.log(categoty[i].itemsOf);
+    if(categoty[i].itemsOf === "food"){
+        const item=$(`
+            <div>
+                    <img src="${categoty[i].img}" alt="kl" class="img">
+                    <br>
+                    <p>${categoty[i].paragraf}</p>
+                    <br>
+                    <p><mark>price:${categoty[i].price} </mark></p>
+                    <br>
+            </div>`)
+            const read=$(`<button>read more</button>`)
+            read.addClass("jqbtn") 
+            read.on("click",()=>
+            
+            {
+
+                info.show()
+                info.html("")
+                hide.hide()
+                container.hide()
+                const information=$(
+                `
+                    <div class="broductPic"><img src=${categoty[i].img} alt=""></div>
+                    <div class="broductinfo">
+                    <h1 class="namename">${categoty[i].paragraf}</h1>
+                    <br>
+                    <br>
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae totam ea doloremque recusandae at aspernatur provident obcaecati beatae voluptas quia rem maxime libero laudantium nihil dicta perferendis, cum sit. Amet!</h4>
+                    <br>
+                    <p>price: <span style="color: darkblue;">${categoty[i].price}</span></p>
+                    </div>
+                `)
+                const bttn = $(`<button class="infobtn">HOME</button>`)
+                info.append(information)
+                $(".broductinfo").append(bttn)
+                
+                bttn.on("click", () => {
+                    info.hide()
+                    hide.show()
+                    container.show()
+                });
+
+            })
+           
+            item.append(read)
+
+
+
+            const addCart=$(`<button>Add To Card</button>`)
+            addCart.addClass("jqbtn") 
+            item.append(addCart)
+            addCart.on("click",()=>
+            {
+                cardsArr.push(categoty[i])    
+                console.log(cardsArr);
+                localStorage.setItem("card" , JSON.stringify(cardsArr))
+                
+            })
+   container.append(item)
+    }
+}
+}
+const items2 = function(){
+for(let i = 0; i < categoty.length; i++){
+    console.log(categoty[i].itemsOf);
+    if(categoty[i].itemsOf === "toys"){
+        const item=$(`
+            <div>
+                    <img src="${categoty[i].img}" alt="kl" class="img">
+                    <br>
+                    <p>${categoty[i].paragraf}</p>
+                    <br>
+                    <p><mark>price:${categoty[i].price} </mark></p>
+                    <br>
+                    <br>
+                    <br>
+            </div>`)
+            const read=$(`<button>read more</button>`)
+            read.addClass("jqbtn") 
+            read.on("click",()=>
+            
+            {
+
+                info.show()
+                info.html("")
+                hide.hide()
+                container.hide()
+                const information=$(
+                `
+                    <div class="broductPic">
+                    <img src=${categoty[i].img} alt="">
+                    </div>
+                    <div class="broductinfo">
+                    <h1 class="namename">${categoty[i].paragraf}</h1>
+                    <br>
+                    <br>
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae totam ea doloremque recusandae at aspernatur provident obcaecati beatae voluptas quia rem maxime libero laudantium nihil dicta perferendis, cum sit. Amet!</h4>
+                    <br>
+                    <p>price: <span style="color: darkblue;">${categoty[i].price}</span></p>
+                    </div>
+                `)
+                const bttn = $(`<button class="infobtn">HOME</button>`)
+                info.append(information)
+                $(".broductinfo").append(bttn)
+                
+                bttn.on("click", () => {
+                    info.hide()
+                    hide.show()
+                    container.show()
+                });
+
+            })
+            item.append(read)
+
+            const addCart=$(`<button>Add To Card</button>`)
+            addCart.addClass("jqbtn") 
+            item.append(addCart)
+            addCart.on("click",()=>
+            {
+                cardsArr.push(categoty[i])    
+                console.log(cardsArr);
+                localStorage.setItem("card" , JSON.stringify(cardsArr))
+                
+            })
+   container.append(item)
+    }
+}
+}
+const items3 = function(){
+for(let i = 0; i < categoty.length; i++){
+    console.log(categoty[i].itemsOf);
+    if(categoty[i].itemsOf === "grooming"){
+        const item=$(`
+        <div class="one">
+                <img src="${categoty[i].img}" alt="kl" class="img">
+                <br>
+                <p>${categoty[i].paragraf}</p>
+                <br>
+                <p><mark>price:${categoty[i].price} </mark></p>
+                <br>
+        </div>`) 
+            const read=$(`<button>read more</button>`)
+            read.addClass("jqbtn") 
+            read.on("click",()=>
+            
+            {
+
+                info.show()
+                info.html("")
+                hide.hide()
+                container.hide()
+                const information=$(
+                `
+                    <div class="broductPic"><img src=${categoty[i].img} alt=""></div>
+                    <div class="broductinfo">
+                    <h1 class="namename">${categoty[i].paragraf}</h1>
+                    <br>
+                    <br>
+                    <h4>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae totam ea doloremque recusandae at aspernatur provident obcaecati beatae voluptas quia rem maxime libero laudantium nihil dicta perferendis, cum sit. Amet!</h4>
+                    <br>
+                    <p>price: <span style="color: darkblue;">${categoty[i].price}</span></p>
+                    </div>
+                `)
+                const bttn = $(`<button class="infobtn">HOME</button>`)
+                info.append(information)
+                $(".broductinfo").append(bttn)
+                
+                bttn.on("click", () => {
+                    info.hide()
+                    hide.show()
+                    container.show()
+                });
+
+            })
+            item.append(read)
+
+
+        const addCart=$(`<button>Add To Card</button>`)
+        addCart.addClass("jqbtn") 
+        item.append(addCart)
+        addCart.on("click",()=>
+        {
+            cardsArr.push(categoty[i])    
+            console.log(cardsArr);
+            localStorage.setItem("card" , JSON.stringify(cardsArr))
+        })
+   container.append(item)
+    }
+}
+}
+
+
+btn2.on("click", () => {
+    container.html("")
+    items()
+  });
+btn3.on("click", () => {
+    container.html("")   
+    items2()
+  });
+btn4.on("click", () => {
+    container.html("")   
+    items3()
+  });
+
+
+
+
+
+
+
+
+
+
+
+const picksitem=$(".picksitem")
+// const baskit=$(".baskit")
+const insidBaskit=$(".insidBaskit")
+
+const theItems = function() {
+    for (let i = 0; i < cardsArr.length; i++) {
+        const cartsCards = $(
+            `
+            <div>
+            <img src="${cardsArr[i].img}" alt="kl" class="img">
+            <br>
+            <p>${cardsArr[i].paragraf}</p>
+            <br>
+            <p><mark>price:${cardsArr[i].price}</mark></p>
+            </div>
+            `
+        )
+        const removeBtn=$(`
+        
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5Zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6Z"/>
+        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1ZM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118ZM2.5 3h11V2h-11v1Z"/>
+        </svg>
+        
+        `)
+        removeBtn.addClass("btnbtn")
+        cartsCards.append(removeBtn)
+        
+        insidBaskit.append(cartsCards)
+        removeBtn.on("click", () => {
+        cardsArr.splice(i,1)
+        console.log(cardsArr);
+        insidBaskit.html("")
+        theItems()
+        localStorage.setItem("card" , JSON.stringify(cardsArr))
+      });
+        
+    }
+   
+}
+
+picksitem.on("click", () => {
+        container.hide()
+        hide.hide()
+        insidBaskit.show()
+        theItems()
+        addMore.show()
+  });
+
+
+
+
+
+
+
+
+const addMore=$(".addMore")
+
+
+  addMore.on("click", () => {
+    container.show()
+    hide.show()
+    insidBaskit.hide()
+    addMore.hide()
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
